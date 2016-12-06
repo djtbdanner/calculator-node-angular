b7dd73fcff83c67e2e284a1f40614452a419b88d
Dave Danner <djtbdanner@yahoo.com>
2016-12-05 19:35:22 -0600

    add debounce and wait spinner

--- app/jdf-calculator.component.js
+++ app/jdf-calculator.component.js
@@ -9,14 +9,52 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var forms_1 = require('@angular/forms');
 var jdf_calculator_classes_1 = require('./jdf-calculator.classes');
 var jdf_calculator_service_1 = require('./jdf-calculator.service');
+require('rxjs/add/operator/debounceTime');
+var core_2 = require('@angular/core');
+core_2.enableProdMode();
 var JDFCalculator = (function () {
     function JDFCalculator(_calculatorService) {
         this._calculatorService = _calculatorService;
+        this.loanAmount = new forms_1.FormControl();
+        this.interestRate = new forms_1.FormControl();
+        this.numberOfPayments = new forms_1.FormControl();
+        this.startDate = new forms_1.FormControl();
         this.loan = new jdf_calculator_classes_1.Loan;
         this.loanItem = "";
     }
+    JDFCalculator.prototype.ngOnInit = function () {
+        var _this = this;
+        var dbTimeMillis = 500;
+        this.loanAmount.valueChanges
+            .debounceTime(dbTimeMillis)
+            .subscribe(function (newValue) {
+            _this.loan.amount = newValue;
+            _this.calculateLoan();
+        });
+        this.interestRate.valueChanges
+            .debounceTime(dbTimeMillis)
+            .subscribe(function (newValue) {
+            _this.loan.interest = newValue;
+            _this.calculateLoan();
+        });
+        this.numberOfPayments.valueChanges
+            .debounceTime(dbTimeMillis)
+            .subscribe(function (newValue) {
+            _this.loan.payments = newValue;
+            _this.calculateLoan();
+        });
+        this.startDate.valueChanges
+            .debounceTime(dbTimeMillis)
+            .subscribe(function (newValue) {
+            var dateArray = newValue.split(/\D/);
+            var date = new Date(parseInt(dateArray[0], 10), parseInt(dateArray[1], 10), parseInt(dateArray[2], 10));
+            _this.loan.startDate = date;
+            _this.calculateLoan();
+        });
+    };
     JDFCalculator.prototype.calculateLoan = function () {
         if (this.loan.amount > 0 && this.loan.interest >= 0 && this.loan.payments > 0) {
             if (this.loan.startDate === undefined) {
@@ -26,23 +64,8 @@ var JDFCalculator = (function () {
             this.payments = this.paymentData.payments;
         }
     };
-    JDFCalculator.prototype.changeLoanAmount = function (value) {
-        this.loan.amount = value;
-        this.calculateLoan();
-    };
-    JDFCalculator.prototype.changeLoanInterest = function (value) {
-        this.loan.interest = value / 100;
-        this.calculateLoan();
-    };
-    JDFCalculator.prototype.changeLoanPayment = function (value) {
-        this.loan.payments = value;
-        this.calculateLoan();
-    };
-    JDFCalculator.prototype.changeStartDate = function (value) {
-        var dateArray = value.split(/\D/);
-        var date = new Date(parseInt(dateArray[0], 10), parseInt(dateArray[1], 10), parseInt(dateArray[2], 10));
-        this.loan.startDate = date;
-        this.calculateLoan();
+    JDFCalculator.prototype.clearSchedule = function () {
+        this.paymentData = null;
     };
     JDFCalculator = __decorate([
         core_1.Component({
--- app/jdf-calculator.component.ts
+++ app/jdf-calculator.component.ts
@@ -1,52 +1,74 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Payment, Loan, PaymentData } from './jdf-calculator.classes';
 import { JDFCalculatorService } from './jdf-calculator.service';
+import 'rxjs/add/operator/debounceTime';
+import {enableProdMode} from '@angular/core';
 
-
+enableProdMode();
 @Component({
-  selector: 'jdf-calculator',
-  templateUrl: 'app/jdf-calculator-form.component.html'
+    selector: 'jdf-calculator',
+    templateUrl: 'app/jdf-calculator-form.component.html'
 })
 export class JDFCalculator {
 
-  payments: Payment[];
-  loan: Loan;
-  paymentData: PaymentData;
-  loanItem:String;
+    payments: Payment[];
+    loan: Loan;
+    paymentData: PaymentData;
+    loanItem: String;
 
-  constructor(private _calculatorService: JDFCalculatorService) {
-    this.loan = new Loan; 
-    this.loanItem="";
-  } 
+    loanAmount = new FormControl();
+    interestRate = new FormControl();
+    numberOfPayments = new FormControl();
+    startDate = new FormControl();
 
-  calculateLoan() {
-    if (this.loan.amount > 0 && this.loan.interest >= 0 && this.loan.payments > 0) {
-      if (this.loan.startDate === undefined) {
-        this.loan.startDate = this._calculatorService.addMonthsUTC(new Date(), 1);
-      }
-      this.paymentData = this._calculatorService.calculate(this.loan);
-      this.payments = this.paymentData.payments;
-    } 
-  }
+    constructor(private _calculatorService: JDFCalculatorService) {
+        this.loan = new Loan;
+        this.loanItem = "";
+    }
 
-  changeLoanAmount(value: number) {
-    this.loan.amount = value;
-    this.calculateLoan();
-  }
-  changeLoanInterest(value: number) {
-    this.loan.interest = value / 100;
-    this.calculateLoan();
-  }
-  changeLoanPayment(value: number) {
-    this.loan.payments = value;
-    this.calculateLoan();
-  }
+    ngOnInit() {
+        let dbTimeMillis = 500;
+        this.loanAmount.valueChanges
+            .debounceTime(dbTimeMillis)
+            .subscribe(newValue => {
+                this.loan.amount = newValue;
+                this.calculateLoan();
+            });
+        this.interestRate.valueChanges
+            .debounceTime(dbTimeMillis)
+            .subscribe(newValue => {
+                this.loan.interest = newValue;
+                this.calculateLoan();
+            });
+        this.numberOfPayments.valueChanges
+            .debounceTime(dbTimeMillis)
+            .subscribe(newValue => {
+                this.loan.payments = newValue;
+                this.calculateLoan(); 
+            });
+        this.startDate.valueChanges
+            .debounceTime(dbTimeMillis)
+            .subscribe(newValue => {
+                let dateArray = newValue.split(/\D/);
+                let date = new Date(parseInt(dateArray[0], 10), parseInt(dateArray[1], 10), parseInt(dateArray[2], 10));
+                this.loan.startDate = date;
+                this.calculateLoan();
+            });
+    }
 
-  changeStartDate(value: String) {
-    let dateArray = value.split(/\D/);
-    let date = new Date(parseInt(dateArray[0], 10), parseInt(dateArray[1], 10), parseInt(dateArray[2], 10));
-    this.loan.startDate = date;
-    this.calculateLoan();
-  }
+    calculateLoan() {
+        if (this.loan.amount > 0 && this.loan.interest >= 0 && this.loan.payments > 0) {
+            if (this.loan.startDate === undefined) {
+                this.loan.startDate = this._calculatorService.addMonthsUTC(new Date(), 1);
+            }
+            this.paymentData = this._calculatorService.calculate(this.loan);
+            this.payments = this.paymentData.payments;
+        }
+    }
+
+    clearSchedule() {
+        this.paymentData = null;
+    }
 
 }
